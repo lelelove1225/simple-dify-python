804e7c5162387816f9c2c3468b86ffc4c80643a6
Charlotte.J.Ikurumi <40084865+lelelove1225@users.noreply.github.com>
2024-11-26 20:37:27 +0900

    initial commit

---
+++ hello.py
@@ -0,0 +1,35 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+# .envファイルを読み込む
+load_dotenv()
+
+# Dify APIのAPIキー
+API_KEY = os.getenv("API_KEY")
+# Dify APIのベースURL
+BASE_URL = "https://api.dify.ai/v1/chat-messages"
+
+
+def get_dify_response(message: str, user_id: str):
+    # リクエストヘッダー
+    headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type": "application/json"}
+    # リクエストボディ
+    data = {
+        "inputs": {},
+        "query": message,
+        "response_mode": "blocking",
+        "user": user_id,
+    }
+
+    # POSTリクエストを送信
+    response = requests.post(BASE_URL, headers=headers, json=data)
+    response.raise_for_status()
+    return response.json()["answer"]
+
+
+if __name__ == "__main__":
+    message = "オーストラリアの首都はどこ"
+    user_id = "test_user"
+    response = get_dify_response(message, user_id)
+    print(response)
